47190f5bd653c182f29747c1799de34f0b1634c0
havi1306 <49012637+havi1306@users.noreply.github.com>
2022-11-25 10:22:59 +0700

    remove webelement

--- src/main/java/pages/GeneralPage.java
+++ src/main/java/pages/GeneralPage.java
@@ -9,10 +9,6 @@ public class GeneralPage {
     // Locators
     String tabMenu = "//span[text()='%s']";
     private final By lblWelcomeMessage = By.xpath("//div[@class='account']/strong");
-    protected WebElement getLblWelcomeMessage() {
-        return Constants.WEBDRIVER.findElement(lblWelcomeMessage);
-    }
-    protected WebElement getTabMenu(String tabName){ return Constants.WEBDRIVER.findElement(By.xpath(String.format(tabMenu,tabName)));}
 
     // Methods
 
